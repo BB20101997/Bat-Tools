c3531833d66dd15d7eee1b02753e7d545bfb5935
BB20101997 <blessmann-bennet@t-online.de>
2016-05-26 00:20:50 +0200

    + added the content of the previouse txt files as comments

--- Copy.bat
+++ Copy.bat
@@ -1,2 +1,15 @@
+REM the copy bat needs 3 command line arguments
+REM examle test0.txt 
+REM if we want 15 copys
+REM the last one will be text15.txt
+REM 
+REM 1 how many times to copy
+REM 2 befor the the number in the example test
+REM 3 after the number here .txt
+REM 
+REM example 15 test .txt 
+REM 
+REM also the file to copy needs to have the number 0 it will be the one that will be copied
+
 FOR /L %%A IN (1 1 %1) DO COPY "%20%3" "%2%%A%3"
 pause
--- Rename.bat
+++ Rename.bat
@@ -1,2 +1,28 @@
+REM the copy bat needs 5 command line arguments
+REM 
+REM examle  test0.txt
+REM 	test1.txt
+REM 	.
+REM 	.
+REM 	.
+REM 	test15.txt
+REM 
+REM to 	Test0.tat
+REM 	Text1.tat
+REM 	.
+REM 	.
+REM 	.
+REM 	Text15.tat
+REM 
+REM 1 highest number rename  
+REM 2 befor the the number in the example test
+REM 3 after the number here .txt
+REM 4 new name
+REM 5 new ending
+REM 
+REM examlpe here : Rename.bat 15 test .txt Test .tat
+REM 
+REM starts whit number 0 but will be ignored if not found.
+
 FOR /L %%A IN (0 1 %1) DO REN "%2%%A%3" "%4%%A%5"
 pause
